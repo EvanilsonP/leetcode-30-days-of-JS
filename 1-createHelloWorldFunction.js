7c3a8e2c0a12e42041c97269f111d482f863e883
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-06 11:20:53 -0300

    Leet code write a hello world function

---
+++ 1-createHelloWorldFunction.js
@@ -0,0 +1,12 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+// Solution
+function createHelloWorld() {
+    return function () {
+        return 'Hello World'
+    }
+}
+
+// output: Hello World
+
+
